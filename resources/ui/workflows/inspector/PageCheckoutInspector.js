1902bc0b155130938769d8a5a431b250473f5635
akulbii <alk@astarios.com>
2024-01-24 12:15:06 +0200

    Implement activity inspectors for BPMN editor
    Tightly connected with that change:
    https://gerrit.wikimedia.org/r/c/mediawiki/extensions/Workflows/+/980828
    
    ERM34416
    
    Depends-On: If33fe63a56915aa9ec4f10fe487d1838fe4a2013
    Change-Id: I5749d20d34348b189c5986524383b03d9fcf4443

---
+++ resources/ui/workflows/inspector/PageCheckoutInspector.js
@@ -0,0 +1,49 @@
+window.pagecheckout = window.pagecheckout || {};
+window.pagecheckout.ui = pagecheckout.ui || {};
+window.pagecheckout.ui.workflows = pagecheckout.ui.workflows || {};
+window.pagecheckout.ui.workflows.inspector = pagecheckout.ui.workflows.inspector || {};
+
+pagecheckout.ui.workflows.inspector.PageCheckoutInspector = function( element, dialog ) {
+	pagecheckout.ui.workflows.inspector.PageCheckoutInspector.parent.call( this, element, dialog );
+};
+
+OO.inheritClass( pagecheckout.ui.workflows.inspector.PageCheckoutInspector, workflows.editor.inspector.ActivityInspector );
+
+pagecheckout.ui.workflows.inspector.PageCheckoutInspector.prototype.getDialogTitle = function() {
+	return mw.message( 'pagecheckout-ui-workflows-inspector-activity-page-checkout-title' ).text();
+};
+
+pagecheckout.ui.workflows.inspector.PageCheckoutInspector.prototype.getItems = function() {
+	return  [
+		{
+			type: 'section_label',
+			title: mw.message( 'workflows-ui-editor-inspector-properties' ).text()
+		},
+		{
+			type: 'user_picker',
+			name: 'properties.user',
+			label: mw.message( 'pagecheckout-ui-workflows-inspector-activity-page-checkout-property-user' ).text(),
+			help: mw.message( 'pagecheckout-ui-workflows-inspector-activity-page-checkout-property-user-help' ).text(),
+			required: true
+		},
+		{
+			type: 'title',
+			name: 'properties.pagename',
+			label: mw.message( 'pagecheckout-ui-workflows-inspector-activity-page-checkout-property-pagename' ).text(),
+			help: mw.message( 'pagecheckout-ui-workflows-inspector-activity-page-checkout-property-pagename-help' ).text()
+		},
+		{
+			type: 'checkbox',
+			name: 'properties.force',
+			label: mw.message( 'pagecheckout-ui-workflows-inspector-activity-page-checkout-property-force' ).text(),
+			help: mw.message( 'pagecheckout-ui-workflows-inspector-activity-page-checkout-property-force-help' ).text()
+		},
+		{
+			type: 'text',
+			name: 'properties.pageId',
+			hidden: true
+		}
+	];
+};
+
+workflows.editor.inspector.Registry.register( 'page_checkout', pagecheckout.ui.workflows.inspector.PageCheckoutInspector );
