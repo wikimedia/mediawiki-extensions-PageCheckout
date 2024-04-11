0132e3b327e9bbdb8f8c415d1ff60289829b6145
hslater <hamish.slater@hallowelt.com>
2024-04-11 20:35:33 +1200

    Convert to abstract schema
    ERM36733
    
    [REL1_39, REL1_39-2.0.x, master]
    
    Change-Id: Ic52822a69d9ee0fd199de37f3235436adceb2728
    (cherry picked from commit 28cb85434b463e71659e97d2fbb7fa40a2cbfb07)
    (cherry picked from commit aabd9674c540508f1bb392d63a3463a8db68b63b)

--- db/page_checkout_locks.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS /*$wgDBprefix*/page_checkout_locks (
-    `pcl_id` int unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    `pcl_page_id` INT NOT NULL,
-    `pcl_user_id` INT NOT NULL,
-    `pcl_payload` BLOB NULL
-) /*$wgDBTableOptions*/;
