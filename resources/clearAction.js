cc51af000c5226d2644cc972343be9da7b297a08
hslater <hamish.slater@hallowelt.com>
2025-03-13 16:35:56 +1300

    eslint
    ERM20742
    
    [REL1_43, master]
    
    Change-Id: I27d380395645e00d7bc21722bf867362c872bfef

--- resources/clearAction.js
+++ resources/clearAction.js
@@ -1,12 +1,12 @@
-$( function() {
-	$( document ).on( 'click', '#ca-pc_clear', function( e ) {
+$( () => {
+	$( document ).on( 'click', '#ca-pc_clear', ( e ) => {
 		e.preventDefault();
-		OO.ui.confirm( mw.msg( 'pagecheckout-ui-clear-confirm' ) ).done( function ( confirmed ) {
+		OO.ui.confirm( mw.msg( 'pagecheckout-ui-clear-confirm' ) ).done( ( confirmed ) => {
 			if ( confirmed ) {
 				new mw.Api().postWithToken( 'csrf', {
 					action: 'pagecheckout-clear',
-					page_title: mw.config.get( 'wgRelevantPageName' )
-				} ).done( function() {
+					page_title: mw.config.get( 'wgRelevantPageName' ) // eslint-disable-line camelcase
+				} ).done( () => {
 					window.location.reload();
 				} );
 			}
