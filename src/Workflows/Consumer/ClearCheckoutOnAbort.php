a4129b8770b2cb76d975a448a51a3cd510094225
hslater <hamish.slater@hallowelt.com>
2025-03-13 16:47:56 +1300

    PHPCS
    ERM20742
    
    [REL1_43, master]
    
    Change-Id: I6f8cf1e98f8c5b24e3b76eb25154e101e3f4b8bb

--- src/Workflows/Consumer/ClearCheckoutOnAbort.php
+++ src/Workflows/Consumer/ClearCheckoutOnAbort.php
@@ -7,8 +7,8 @@ use EventSauce\EventSourcing\Message;
 use MediaWiki\Extension\PageCheckout\CheckoutManager;
 use MediaWiki\Extension\Workflows\Storage\Event\WorkflowAborted;
 use MediaWiki\MediaWikiServices;
-use MediaWiki\Title\Title;
 use MediaWiki\Message\Message as MediaWikiMessage;
+use MediaWiki\Title\Title;
 use MWException;
 
 class ClearCheckoutOnAbort implements Consumer {
